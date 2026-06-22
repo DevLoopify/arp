112e4147be14fb40480c8bea0f756227b8c505ef
ChrisWarzecha <43755185+ChrisWarzecha@users.noreply.github.com>
2026-06-22 19:49:05 +0200

    Irgendwas Blabla. Closes #9

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Test 2.</Text>      
+      <Text style={styles.title}>Test Chris.</Text>      
     </View>
   );
 }
