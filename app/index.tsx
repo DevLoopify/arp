cddfff9f094946cf64541404bd8455859f4d82a6
Ben <yodamaddog@gmail.com>
2026-06-22 12:18:57 +0200

    add test label (#3)

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Start building.</Text>      
+      <Text style={styles.title}>Test.</Text>      
     </View>
   );
 }
