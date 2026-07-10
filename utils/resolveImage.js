fdd7129291aaa021399ec9c45ad21439ba9e9596
Ben <yodamaddog@gmail.com>
2026-07-10 23:22:43 +0200

    refactor resolve image function

--- utils/resolveImage.js
+++ utils/resolveImage.js
@@ -1,21 +1,5 @@
-const imageMap = {
-    'Unknown.jpg': require('../assets/images/Unknown.jpg'),
-    'Unknown-2.jpg': require('../assets/images/Unknown-2.jpg'),
-    'Unknown-3.jpg': require('../assets/images/Unknown-3.jpg'),
-    'Unknown-4.jpg': require('../assets/images/Unknown-4.jpg'),
-    'Unknown-5.jpg': require('../assets/images/Unknown-5.jpg'),
-    'Unknown-6.jpg': require('../assets/images/Unknown-6.jpg'),
-    'Unknown-7.jpg': require('../assets/images/Unknown-7.jpg'),
-    'Unknown-8.jpg': require('../assets/images/Unknown-8.jpg'),
-    'Unknown-9.jpg': require('../assets/images/Unknown-9.jpg'),
-    'unnamed.jpg': require('../assets/images/unnamed.jpg'),
-    'Luisenplatz Darmstadt Innenstadt Entwicklungskonzept 09062023.webp': require('../assets/images/Luisenplatz Darmstadt Innenstadt Entwicklungskonzept 09062023.webp'),
-};
-
 import { resolveApiUrl } from './api';
 
 export function resolveImage(path) {
-    const filename = path.split('/').pop();
-    if (imageMap[filename]) return imageMap[filename];
     return { uri: resolveApiUrl(path) };
 }
